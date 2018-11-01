928ccd54ea07159cbb602078f8d9c3c4a28c6f7f
Ken Murrell <kmurrell@markettrack.com>
2018-11-01 13:17:35 -0400

    008

--- DataManager.py
+++ DataManager.py
@@ -1,6 +1,31 @@
 import psycopg2
+import regex as re
+
+# NOTE: the table name is "review" you dont need to specify the database name
+def retrieve(query):
+    try:
+        conn = psycopg2.connect(database="assortment",
+                                user="assortment_readonly_user",
+                                password="I82aOqmZNwj4fIrH",
+                                host="core-prod-gameplan-rds.360pi.com",
+                                port="5432")
+        cur = conn.cursor()
+        cur.execute(_add_limit(query))
+        rows = cur.fetchall()
+    except psycopg2.Error as e:
+        print(e.pgerror)
+        conn.close()
+        return None
+    else:
+        cur.close()
+        conn.close()
+        return rows
 
 
+def _add_limit(query):
+    query = re.sub(r"limit[\s][0-9]+", "", query)
+    query += " limit 100"
+    return query
 
-def connect():
-    conn = psycopg2.connect(database="testdb", user="assortment_readonly_user", password="I82aOqmZNwj4fIrH", host="127.0.0.1", port="5432")
+
+#print(retrieve("SELECT * from review"))
